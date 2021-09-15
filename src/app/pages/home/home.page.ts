1856215f47f42f7abe0f2e669a44c2cd3ac805e1
AmIEviil <lokiinn@gmail.com>
2021-09-14 20:55:00 -0400

    confirmar

---
+++ src/app/pages/home/home.page.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+
+import { FormControl,FormGroup } from '@angular/forms';
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  valor :String="Hola Mundo";
+  constructor() {}
+
+persona = new FormGroup({
+  elUsuario : new FormControl(''),
+  elContraseña : new FormControl('')
+})
+//Creacion de arreglo
+lista_persona = new Array ();
+perso:any;
+
+grabar(){
+    this.perso = {
+        usuario:this.persona.controls.elUsuario.value,
+        contraseña:this.persona.controls.elContraseña.value,
+    };
+    //console.log(this.perso);
+    this.lista_persona.push(this.perso);
+    var datos = this.lista_persona;
+    localStorage.setItem('misdatos',JSON.stringify(datos));
+    alert("Grabo");
+}
+eliminar(){}
+
+listar(){}
+
+}
---
+++ src/app/pages/login/login.page.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  constructor(private router:Router, private alertController: AlertController) { }
+
+  ngOnInit() {
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: 'This is an alert message.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+  }
+}
